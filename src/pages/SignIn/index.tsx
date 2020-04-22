b7001b11be2f87b0efbbda720396e677044d2269
guicostaarantes <arantes.g@outlook.com>
2020-04-22 19:52:50 -0300

    added validation for sign in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,16 +16,31 @@ import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-  const handleSubmit = useCallback((data) => {
-    // TODO: add logic
-    console.log(data);
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data) => {
+    try {
+      // eslint-disable-next-line no-unused-expressions
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Email inválido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      // eslint-disable-next-line no-unused-expressions
+      formRef.current?.setErrors(errors);
+    }
   }, []);
 
   return (
     <Container>
       <Content>
         <img src={logo} alt="logo" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="email" icon={FiMail} placeholder="Email" />
           <Input
